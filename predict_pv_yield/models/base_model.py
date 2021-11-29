d8c66e7a559e11a02f8d6d5aac6b2c29b18a20fa
peterdudfield <peter.dudfield@hotmail.com>
2021-11-29 08:17:05 +0000

    make code tidier

--- predict_pv_yield/models/base_model.py
+++ predict_pv_yield/models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel(pl.LightningModule):
     batch_size = 32
 
     # results file name
-    results_file_name = 'results_epoch'
+    results_file_name = "results_epoch"
 
     # list of results dataframes. This is used to save validation results
     results_dfs = []
@@ -37,16 +37,26 @@ class BaseModel(pl.LightningModule):
     def __init__(self):
         super().__init__()
 
-        self.history_len_5 = self.history_minutes // 5  # the number of historic timestemps for 5 minutes data
-        self.forecast_len_5 = self.forecast_minutes // 5  # the number of forecast timestemps for 5 minutes data
+        self.history_len_5 = (
+            self.history_minutes // 5
+        )  # the number of historic timestemps for 5 minutes data
+        self.forecast_len_5 = (
+            self.forecast_minutes // 5
+        )  # the number of forecast timestemps for 5 minutes data
 
-        self.history_len_30 = self.history_minutes // 30  # the number of historic timestemps for 5 minutes data
-        self.forecast_len_30 = self.forecast_minutes // 30  # the number of forecast timestemps for 5 minutes data
+        self.history_len_30 = (
+            self.history_minutes // 30
+        )  # the number of historic timestemps for 5 minutes data
+        self.forecast_len_30 = (
+            self.forecast_minutes // 30
+        )  # the number of forecast timestemps for 5 minutes data
 
         # the number of historic timesteps for 60 minutes data
         # Note that ceil is taken as for 30 minutes of history data, one history value will be used
         self.history_len_60 = int(np.ceil(self.history_minutes / 60))
-        self.forecast_len_60 = self.forecast_minutes // 60 # the number of forecast timestemps for 60 minutes data
+        self.forecast_len_60 = (
+            self.forecast_minutes // 60
+        )  # the number of forecast timestemps for 60 minutes data
 
         if not hasattr(self, "output_variable"):
             print("setting")
@@ -64,7 +74,7 @@ class BaseModel(pl.LightningModule):
 
         self.weighted_losses = WeightedLosses(forecast_length=self.forecast_len)
 
-    def _training_or_validation_step(self, batch, tag: str):
+    def _training_or_validation_step(self, batch, tag: str, return_model_outputs: bool = False):
         """
         batch: The batch data
         tag: either 'Train', 'Validation' , 'Test'
@@ -76,9 +86,8 @@ class BaseModel(pl.LightningModule):
         # put the batch data through the model
         y_hat = self(batch)
 
-
         # get the true result out. Select the first data point, as this is the pv system in the center of the image
-        if self.output_variable == 'gsp_yield':
+        if self.output_variable == "gsp_yield":
             y = batch.gsp.gsp_yield
         else:
             y = batch.pv.pv_yield
@@ -96,7 +105,12 @@ class BaseModel(pl.LightningModule):
         # shape (2, num_timesteps))[0, 1] on each example, and taking
         # the mean across the batch?
         self.log_dict(
-            {f"MSE/{tag}": mse_loss, f"NMAE/{tag}": nmae_loss, f"MSE_EXP/{tag}": mse_exp, f"MAE_EXP/{tag}": mae_exp},
+            {
+                f"MSE/{tag}": mse_loss,
+                f"NMAE/{tag}": nmae_loss,
+                f"MSE_EXP/{tag}": mse_exp,
+                f"MAE_EXP/{tag}": mae_exp,
+            },
             on_step=True,
             on_epoch=True,
             sync_dist=True  # Required for distributed training
@@ -125,7 +139,10 @@ class BaseModel(pl.LightningModule):
                 # (even multi-GPU on signle machine).
             )
 
-        return nmae_loss
+        if return_model_outputs:
+            return nmae_loss, y_hat
+        else:
+            return nmae_loss
 
     def training_step(self, batch, batch_idx):
 
@@ -139,12 +156,14 @@ class BaseModel(pl.LightningModule):
         if type(batch) == dict:
             batch = BatchML(**batch)
 
+        # get model outputs
+        nmae_loss, model_output = self._training_or_validation_step(
+            batch, tag="Validation", return_model_outputs=True
+        )
+
         INTERESTING_EXAMPLES = (1, 5, 6, 7, 9, 11, 17, 19)
         name = f"validation/plot/epoch_{self.current_epoch}_{batch_idx}"
         if batch_idx in [0, 1, 2, 3, 4]:
-
-            # get model outputs
-            model_output = self(batch)
 
             # make sure the interesting example doesnt go above the batch size
             INTERESTING_EXAMPLES = (i for i in INTERESTING_EXAMPLES if i < self.batch_size)
@@ -155,12 +174,12 @@ class BaseModel(pl.LightningModule):
                     fig = plot_example(
                         batch,
                         model_output,
-                        history_minutes=self.history_len_5*5,
-                        forecast_minutes=self.forecast_len_5*5,
+                        history_minutes=self.history_len_5 * 5,
+                        forecast_minutes=self.forecast_len_5 * 5,
                         nwp_channels=NWP_VARIABLE_NAMES,
                         example_i=example_i,
                         epoch=self.current_epoch,
-                        output_variable=self.output_variable
+                        output_variable=self.output_variable,
                     )
 
                     # save fig to log
@@ -173,25 +192,29 @@ class BaseModel(pl.LightningModule):
 
             # 2. plot summary batch of predictions and results
             # make x,y data
-            if self.output_variable == 'gsp_yield':
-                y = batch.gsp.gsp_yield[0: self.batch_size, :, 0].cpu().numpy()
+            if self.output_variable == "gsp_yield":
+                y = batch.gsp.gsp_yield[0 : self.batch_size, :, 0].cpu().numpy()
             else:
                 y = batch.pv.pv_yield[0 : self.batch_size, :, 0].cpu().numpy()
             y_hat = model_output[0 : self.batch_size].cpu().numpy()
             time = [
-                pd.to_datetime(x, unit="ns") for x in batch.gsp.gsp_datetime_index[0 : self.batch_size].cpu().numpy()
+                pd.to_datetime(x, unit="ns")
+                for x in batch.gsp.gsp_datetime_index[0 : self.batch_size].cpu().numpy()
             ]
             time_hat = [
                 pd.to_datetime(x, unit="ns")
-                for x in batch.gsp.gsp_datetime_index[0 : self.batch_size, self.history_len_30 + 1 :].cpu().numpy()
+                for x in batch.gsp.gsp_datetime_index[
+                    0 : self.batch_size, self.history_len_30 + 1 :
+                ]
+                .cpu()
+                .numpy()
             ]
 
             # plot and save to logger
             fig = plot_batch_results(model_name=self.name, y=y, y_hat=y_hat, x=time, x_hat=time_hat)
-            fig.write_html(f"temp.html")
+            fig.write_html(f"temp_{batch_idx}.html")
             try:
-                self.logger.experiment[-1][f'validation/plot/{self.current_epoch}_{batch_idx}'].upload(
-                    f"temp.html")
+                self.logger.experiment[-1][name].upload(f"temp_{batch_idx}.html")
             except:
                 pass
 
@@ -200,27 +223,34 @@ class BaseModel(pl.LightningModule):
         # t0_datetime_utc, gsp_id,
         # prediction_0, prediction_1, .....
         # truth_0, truth_1, ....
-        # get model outputs
-        model_output = self(batch).cpu().numpy()
-        results = pd.DataFrame(model_output,
-                               columns=[f'prediction_{i}' for i in range(model_output.shape[1])])
-        results.index.name = 'example_index'
-        for i in range(model_output.shape[1]):
-            results[f'truth_{i}'] = batch.gsp.gsp_yield[:, -self.forecast_len_30 + i, 0].cpu()
-        results['t0_datetime_utc'] = batch.metadata.t0_datetime_utc
-        results['gsp_id'] = batch.gsp.gsp_id[:,0].cpu()
-        results['batch_index'] = batch_idx
 
-        # append
+        # get model outputs and truths
+        predictions_tf = model_output.cpu().numpy()
+        truths_tf = batch.gsp.gsp_yield[:, -self.forecast_len_30:, 0].cpu()
+        predictions = pd.DataFrame(
+            predictions_tf, columns=[f"prediction_{i}" for i in range(predictions_tf.shape[1])]
+        )
+        truths = pd.DataFrame(truths_tf, columns=[f"truth_{i}" for i in range(truths_tf.shape[1])])
+
+        # join truths and predictions
+        results = pd.concat([predictions, truths], axis=1, join="inner")
+        results.index.name = "example_index"
+
+        # add metadata
+        results["t0_datetime_utc"] = batch.metadata.t0_datetime_utc
+        results["gsp_id"] = batch.gsp.gsp_id[:, 0].cpu()
+        results["batch_index"] = batch_idx
+
+        # append so in 'validation_epoch_end' the file is saved
         if batch_idx == 0:
             self.results_dfs = []
         self.results_dfs.append(results)
 
-        return self._training_or_validation_step(batch, tag="Validation")
+        return nmae_loss
 
     def validation_epoch_end(self, outputs):
 
-        logger.info('Saving results of validation to logger')
+        logger.info("Saving results of validation to logger")
 
         # join all validation step results together
         results_df = pd.concat(self.results_dfs)
@@ -232,7 +262,9 @@ class BaseModel(pl.LightningModule):
 
         # upload csv to neptune
         try:
-            self.logger.experiment[-1][f'validation/results/epoch_{self.current_epoch}'].upload(name_csv)
+            self.logger.experiment[-1][f"validation/results/epoch_{self.current_epoch}"].upload(
+                name_csv
+            )
         except:
             pass
 
