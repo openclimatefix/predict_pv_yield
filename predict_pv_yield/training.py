d457d07bc7451219fea989f8449ac53f9676786b
peterdudfield <peter.dudfield@hotmail.com>
2021-12-06 09:22:00 +0000

    add test datamodel back in

--- predict_pv_yield/training.py
+++ predict_pv_yield/training.py
@@ -85,7 +85,7 @@ def train(config: DictConfig) -> Optional[float]:
     # Evaluate model on test set, using the best model achieved during training
     if config.get("test_after_training") and not config.trainer.get("fast_dev_run"):
         log.info("Starting testing!")
-        trainer.test()
+        trainer.test(model=model, datamodule=datamodule)
 
     # Make sure everything closed properly
     log.info("Finalizing!")
