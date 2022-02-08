488874fe3246cf8dc698b1bca5f89601da7af678
Jack Kelly <jack@openclimatefix.org>
2022-02-08 19:39:03 +0000

    plot JUST predicted PV, but without datetimes, and without label, linewidth, or color

--- notebooks/51.45_just_plotting.py
+++ notebooks/51.45_just_plotting.py
@@ -836,15 +836,15 @@ def plot_timeseries(batch: dict[str, torch.Tensor], network_output: dict[str, to
 
         # Plot historical PV yield
         historical_pv_datetimes = pd.date_range(t0_datetime - pd.Timedelta("30 minutes"), periods=7, freq="5 min")
-        plot_probs(
-            pi=network_output[PI],
-            mu=network_output[MU],
-            sigma=network_output[SIGMA],
-            ax=ax,
-            left=mdates.date2num(forecast_datetimes[0]) - FIFTEEN_MINUTES,
-            right=mdates.date2num(forecast_datetimes[-1]) + FIFTEEN_MINUTES,
-            example_i=example_i,
-        )
+        # plot_probs(
+        #     pi=network_output[PI],
+        #     mu=network_output[MU],
+        #     sigma=network_output[SIGMA],
+        #     ax=ax,
+        #     left=mdates.date2num(forecast_datetimes[0]) - FIFTEEN_MINUTES,
+        #     right=mdates.date2num(forecast_datetimes[-1]) + FIFTEEN_MINUTES,
+        #     example_i=example_i,
+        # )
         # ax.plot(
         #     historical_pv_datetimes,
         #     historical_pv[example_i],
@@ -863,6 +863,7 @@ def plot_timeseries(batch: dict[str, torch.Tensor], network_output: dict[str, to
         # Plot prediction for GSP PV yield and actual GSP PV yield
         # ax.plot(forecast_datetimes, predicted[example_i], label="Predicted GSP PV", color="orange", linewidth=3, alpha=0.8)
         # ax.plot(forecast_datetimes, actual[example_i], label="Actual GSP PV", linewidth=3, alpha=0.8)
+        ax.plot(predicted[example_i])
         
         # Plot NWP params:
         # if "nwp" in batch:
